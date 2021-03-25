317bce1ed135afa3cb40ce0e3efd840c2cb3fc95
IshigeU <theishigedeluxe@gmail.com>
2021-03-25 23:42:57 +0900

    profileモデルの作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :tweets
+  has_one :profile
 
   validates :name, presence: true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
   #get 'homes/index'
   root to: "homes#index"
   resources :users, only: [:new, :edit, :update, :show]
   resources :tweets do
     resources :comments, only: :create
   end
+  resources :account, only: [:show]
 end
