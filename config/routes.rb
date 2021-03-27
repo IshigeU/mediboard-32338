8d28fe87da7dbe4cf53da280f92fad2a7c4c8970
IshigeU <theishigedeluxe@gmail.com>
2021-03-27 22:34:10 +0900

    プロフィールcommit④

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  #get 'users#show'
+  #get 'homes/index'
   root to: "homes#index"
   resources :users, only: [:new, :edit, :update, :show]
   resources :tweets do
     resources :comments, only: :create
   end
   resources :profiles, only: [:show]
+
 end
