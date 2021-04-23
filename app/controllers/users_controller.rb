eabb41c1c111e2d9333d734dead563504f6e5547
IshigeU <theishigedeluxe@gmail.com>
2021-04-24 01:15:47 +0900

    Viewの編集4_23

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,21 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(map_params)
+    if @user.save
+      redirect_to root_path
+    else
+      @users = Map.all
+      render 'users/index'
+    end
+  end
+
+  private
+
+  # ストロングパラメーター
+  def map_params
+    params.require(:user).permit(:name, :email, :age_id, :sex_id, :prefecture_id, :shop_name, :shop_info, :content, :address, :job_id, :image, :latitude, :longitude)
+  end
+
 end
