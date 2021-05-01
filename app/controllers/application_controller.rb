3c875172696af31afc901773c8f868cde8f7d807
IshigeU <theishigedeluxe@gmail.com>
2021-05-02 00:01:53 +0900

    userモデルのテスト完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,9 @@
 class ApplicationController < ActionController::Base
-  #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :age_id, :sex_id, :prefecture_id, :shop_name, :shop_info, :content, :address, :job_id, :image, :latitude, :longitude ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :email, :age_id, :sex_id, :prefecture_id, :shop_name, :shop_info, :content, :address, :job_id, :image, :latitude, :longitude])
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(map_params)
+    @user = User.new(user_params)
     if @user.save
       redirect_to root_path
     else
-      @users = Map.all
+      @users = User.all
       render 'users/index'
     end
   end
@@ -24,8 +24,8 @@ class UsersController < ApplicationController
   private
 
   # ストロングパラメーター
-  def map_params
-    params.require(:user).permit(:name, :email, :age_id, :sex_id, :prefecture_id, :shop_name, :shop_info, :content, :address, :job_id, :image, :latitude, :longitude)
+  def user_params
+    params.require(:user).permit(:name, :email, :age_id, :sex_id, :prefecture_id, :shop_name, :shop_info, :content, :address, :job_id, :image, :latitude, :longitude, :user_id)
   end
 
 end
